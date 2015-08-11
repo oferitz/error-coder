81c45d2f55f6367af315490c77d555befb7661b8
oferitz <ofer.webdev@gmail.com>
2015-08-12 00:16:58 +0300

    major commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 var Octcode = require('./lib/octcode');
-
-var oc = new Octcode('BOLBOL', {delimiter: '***'});
-oc.add(400, 'missing username');
-oc.add(401, 'missing timestamp');
-oc.add(402, 'missing gss');
-console.log('GET:', oc.get(500));
-//console.log(oc.add(401));
-//console.log(oc.add(404));
